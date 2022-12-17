c664030b0182d3f623aaac9be4eb1f4746cdf65d
sky81219 <=>
2022-12-18 02:16:53 +0900

    토픽명 변경

--- consumer.py
+++ consumer.py
@@ -1,12 +1,12 @@
 from kafka import KafkaConsumer
-from typing import Final
+from typing import Final, List
 import json
 
 
 
-TOPIC_NAME: Final[str] = "TRADEBITCOINTOTAL"
-bootstrap_server = ["127.0.0.1:9091", "127.0.0.1:9092", "127.0.0.1:9093"]
-consumer = KafkaConsumer(TOPIC_NAME, bootstrap_servers=bootstrap_server, security_protocol="PLAINTEXT")
+BIT_TOPIC_NAME: Final[str] = "trade_bitcoin_total"
+bootstrap_server: List[str] = ["kafka1:19091", "kafka2:29092", "kafka3:39093"]
+consumer = KafkaConsumer(BIT_TOPIC_NAME, bootstrap_servers=bootstrap_server, security_protocol="PLAINTEXT")
 
 for index in (json.loads(i.value.decode()) for i in consumer):
     print(index)
